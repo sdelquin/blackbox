98438899939974211090b1a2a6d9a2ae35b77240
Sergio Delgado Quintero <sdelquin@gmail.com>
2022-07-31 19:52:36 +0100

    Add cli option to set the size of rubik cubes

--- reto02/main.py
+++ reto02/main.py
@@ -1,5 +1,17 @@
+import typer
 from core import rubik
 
-cube = rubik.RubikCube(cube_size=3)
-cube.gen_random()
-print(cube)
+app = typer.Typer(add_completion=False)
+
+
+@app.command()
+def run(
+    cube_size: int = typer.Option(3, '--cube-size', '-s', help='Size of the Rubik Cube')
+):
+    cube = rubik.RubikCube(cube_size=cube_size)
+    cube.gen_random()
+    print(cube)
+
+
+if __name__ == "__main__":
+    app()
